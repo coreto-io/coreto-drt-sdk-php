580c21e48e1bf90f021536fcea1c64bf0e8174dc
gasher <robert.vintila@algotech.solutions>
2022-11-29 13:44:23 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,243 @@
+# Coreto Ledger SDK
+
+An SDK used to interact with the Coreto Trust and Performance Ledger
+
+## Dependencies
+
+* PHP >=7.4
+
+## Installation via Composer
+
+To install simply run:
+
+```
+composer require coreto/coreto-ledger-sdk
+```
+
+Or add it to `composer.json` manually:
+
+```json
+{
+    "require": {
+        "coreto/coreto-ledger-sdk": "~1"
+    }
+}
+```
+
+## Direct usage
+
+```php
+use Coreto\CoretoLedgerSDK\Client;
+
+$client = new Client(
+  $apiUrl, // The base URL of the API
+  $callerAccountId, // The account ID of the caller (must be a registered source)
+  $callerPrivateKey // The private key of the caller
+);
+```
+
+### Save actions batch
+```php
+$response = $client->saveActionsBatch([
+  'batch' => [[
+    'account_did' => 'did:example:example1',
+    'action_date' => 1669121362000,
+    'action_type' => 'react',
+    'trust' => 1,
+    'performance' => 0.3,
+    'identifier' => 123
+  ], [
+    'account_did' => 'did:example:example2',
+    'action_date' => 1669121369000,
+    'action_type' => 'comment',
+    'trust' => 0.7,
+    'performance' => 1,
+    'identifier' => 124
+  ]]
+]);
+// [
+//   'hash' => '8cAXpSnCyoUTmtE32wrzVuldgC9ro3EHLNy4EzRXQUkg'
+// ]
+// on error returns null
+// on error returns null
+```
+
+### Sava action
+```php
+$response = $client->saveAction([
+    'account_did' => 'did:example:example',
+    'action_date' => 1669121362000,
+    'action_type' => 'react',
+    'trust' => 1,
+    'performance' => 1,
+    'identifier' => 123
+]);
+// [
+//   'hash' => '8cAXpSnCyoUTmtE32wrzVuldgC9ro3EHLNy4EzRXQUkg'
+// ]
+// on error returns null
+```
+
+### Get user actions
+```php
+$response = $client->getUserActions([
+  'source_label' => 'example',
+  'account_did' => 'did:example:example',
+]);
+// [{
+//   'account_did' => 'did:example:example1',
+//   'action_date' => 1669121362000,
+//   'action_type' => 'react',
+//   'trust' => 1,
+//   'performance' => 0.3,
+//   'identifier' => 123
+// }, {
+//   'account_did' => 'did:example:example2',
+//   'action_date' => 1669121369000,
+//   'action_type' => 'comment',
+//   'trust' => 0.7,
+//   'performance' => 1,
+//   'identifier' => 124
+// }]
+```
+
+### Get user trust actions
+```php
+$response = $client->getUserTrustActions([
+  'source_label' => 'example',
+  'account_did' => 'did:example:example',
+]);
+// [{
+//   'account_did' => 'did:example:example1',
+//   'action_date' => 1669121362000,
+//   'action_type' => 'react',
+//   'trust' => 1,
+//   'performance' => 0,
+//   'identifier' => 123
+// }, {
+//   'account_did' => 'did:example:example2',
+//   'action_date' => 1669121369000,
+//   'action_type' => 'comment',
+//   'trust' => 0.7,
+//   'performance' => 0,
+//   'identifier' => 124
+// }]
+```
+
+### Get user performance actions
+```php
+$response = $client->getUserPerformanceActions([
+  'source_label' => 'example',
+  'account_did' => 'did:example:example',
+]);
+// [{
+//   'account_did' => 'did:example:example1',
+//   'action_date' => 1669121362000,
+//   'action_type' => 'react',
+//   'trust' => 0,
+//   'performance' => 0.9,
+//   'identifier' => 123
+// }, {
+//   'account_did' => 'did:example:example2',
+//   'action_date' => 1669121369000,
+//   'action_type' => 'comment',
+//   'trust' => 0,
+//   'performance' => 0.2,
+//   'identifier' => 124
+// }]
+```
+
+### Get source action types
+```php
+$response = $client->getSourceActionTypes([
+  'source' => 'source.near'
+]);
+// ['react', 'comment', 'opinion']
+```
+
+### Create DID
+```php
+$response = $client->createDID();
+// [
+//   'did' => 'did:example:example',
+//   'seed' => 'seed_example'
+// ]
+// on error returns null
+```
+
+### Put DID
+```php
+$response = $client->putDID([
+  'account_id' => 'example.near',
+  'did' => 'did:example:example'
+]);
+// [
+//   'hash' => '8cAXpSnCyoUTmtE32wrzVuldgC9ro3EHLNy4EzRXQUkg'
+// ]
+// on error returns null
+```
+
+### Get DID
+```php
+$response = $client->getDID([
+  'account_id' => 'example.near'
+]);
+// did:example:example
+// on error returns null
+```
+
+### Has DID
+```php
+$response = $client->hasDID([
+  'account_id' => 'example.near'
+]);
+// true
+// on error returns null
+```
+
+### Transfer DID
+```php
+$response = $client->transferDID([
+  'old_account_id' => 'old_example.near',
+  'new_account_id' => 'new_example.near',
+]);
+// [
+//   'hash' => '8cAXpSnCyoUTmtE32wrzVuldgC9ro3EHLNy4EzRXQUkg'
+// ]
+// on error returns null
+```
+
+### Get current block height
+```php
+$response = $client->getCurrentBlockHeight();
+// 105783543
+// on error returns -1
+```
+
+### Get account balance
+```php
+$response = $client->getBalance('example.near');
+// 25
+// on error returns null
+```
+
+## Errors
+```php
+// Smart contract error
+[
+  'error' => 'The error resulted from the smart contract',
+  'type' => 'The error type resulted from the smart contract'
+]
+
+// Internal server error
+[
+  'error' => 'The error resulted from the server',
+  'statusCode' => 'The error status code resulted from the server',
+  'message' => 'The error message resulted from the server'
+]
+```
+
+## Todo
+
+1. Add unit and integration tests
+2. Update documentation
---
+++ src/Client.php
@@ -0,0 +1,206 @@
+<?php
+
+namespace Coreto\CoretoLedgerSDK;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\Response;
+
+class Client
+{
+    private $apiUrl;
+    private $callerAccountId;
+    private $callerPrivateKey;
+
+    public function __construct(
+        string $apiUrl,
+        string $callerAccountId,
+        string $callerPrivateKey
+    ) {
+        $this->apiUrl = $apiUrl;
+        $this->callerAccountId = $callerAccountId;
+        $this->callerPrivateKey = $callerPrivateKey;
+    }
+
+    private function isResponseStatusOk(Response $response) : bool {
+        switch ($response->status()) {
+            case 200:
+                $json = $response->json();
+
+                if (isset($json['error'])) {
+                    return false;
+                }
+
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public function saveActionsBatch($payload) {
+        $url = rtrim($this->apiUrl, '/') . '/ledger/save_actions_batch';
+
+        $payload['caller_account_id'] = $this->callerAccountId;
+        $payload['caller_private_key'] = $this->callerPrivateKey;
+
+        $response = Http::post($url, $payload);
+
+        if (!$this->isResponseStatusOk($response)) {
+            return null;
+        }
+
+        return $response->json();
+    }
+
+    public function saveAction($payload) {
+        $url = rtrim($this->apiUrl, '/') . '/ledger/save_action';
+
+        $payload['caller_account_id'] = $this->callerAccountId;
+        $payload['caller_private_key'] = $this->callerPrivateKey;
+
+        $response = Http::post($url, $payload);
+
+        if (!$this->isResponseStatusOk($response)) {
+            return null;
+        }
+
+        return $response->json();
+    }
+
+    public function getUserActions($payload) {
+        $url = rtrim($this->apiUrl, '/') . '/ledger/get_user_actions';
+
+        $response = Http::post($url, $payload);
+
+        if (!$this->isResponseStatusOk($response)) {
+            return [];
+        }
+
+        return $response->json();
+    }
+
+    public function getUserTrustActions($payload) {
+        $url = rtrim($this->apiUrl, '/') . '/ledger/get_user_trust_actions';
+
+        $response = Http::post($url, $payload);
+
+        if (!$this->isResponseStatusOk($response)) {
+            return [];
+        }
+
+        return $response->json();
+    }
+
+    public function getUserPerformanceActions($payload) {
+        $url = rtrim($this->apiUrl, '/') . '/ledger/get_user_performance_actions';
+
+        $response = Http::post($url, $payload);
+
+        if (!$this->isResponseStatusOk($response)) {
+            return [];
+        }
+
+        return $response->json();
+    }
+
+    public function getSourceActionTypes($payload) {
+        $url = rtrim($this->apiUrl, '/') . '/ledger/get_source_action_types';
+
+        $response = Http::post($url, $payload);
+
+        if (!$this->isResponseStatusOk($response)) {
+            return [];
+        }
+
+        return $response->json();
+    }
+
+    public function createDID($payload) {
+        $url = rtrim($this->apiUrl, '/') . '/did/create_did';
+
+        $response = Http::post($url, $payload);
+
+        if (!$this->isResponseStatusOk($response)) {
+            return null;
+        }
+
+        return $response->json();
+    }
+
+    public function putDID($payload) {
+        $url = rtrim($this->apiUrl, '/') . '/did/put_did';
+
+        $payload['caller_account_id'] = $this->callerAccountId;
+        $payload['caller_private_key'] = $this->callerPrivateKey;
+
+        $response = Http::post($url, $payload);
+
+        if (!$this->isResponseStatusOk($response)) {
+            return null;
+        }
+
+        return $response->json();
+    }
+
+    public function getDID($payload) {
+        $url = rtrim($this->apiUrl, '/') . '/did/get_did';
+
+        $response = Http::post($url, $payload);
+
+        if (!$this->isResponseStatusOk($response)) {
+            return null;
+        }
+
+        return $response->json();
+    }
+
+    public function hasDID($payload) {
+        $url = rtrim($this->apiUrl, '/') . '/did/has_did';
+
+        $response = Http::post($url, $payload);
+
+        if (!$this->isResponseStatusOk($response)) {
+            return null;
+        }
+
+        return $response->json();
+    }
+
+    public function transferDID($payload) {
+        $url = rtrim($this->apiUrl, '/') . '/did/transfer_did';
+
+        $payload['caller_account_id'] = $this->callerAccountId;
+        $payload['caller_private_key'] = $this->callerPrivateKey;
+
+        $response = Http::post($url, $payload);
+
+        if (!$this->isResponseStatusOk($response)) {
+            return null;
+        }
+
+        return $response->json();
+    }
+
+    public function getCurrentBlockHeight() {
+        $url = rtrim($this->apiUrl, '/') . '/current_block_height';
+
+        $response = Http::get($url);
+
+        if (!$this->isResponseStatusOk($response)) {
+            return -1;
+        }
+
+        return $response->json()['block_height'];
+    }
+
+    public function getBalance($accountId) {
+        $url = rtrim($this->apiUrl, '/') . "/balance/{$accountId}";
+
+        $response = Http::get($url);
+
+        if (!$this->isResponseStatusOk($response)) {
+            return null;
+        }
+
+        return $response->json();
+    }
+}
